5533384381ee0ae15ebeb0a7b452448b80a29feb
Airam Toscano <airamlobato@gmail.com>
2022-06-03 19:18:27 -0400

    fix: heroku

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ require('dotenv').config();
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
+const { verifyBollean } = require('./tests/util');
 
 const app = express();
-
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
-);
-const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
-);
-//
-app.use(cors());
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-// console.log(hereIsTheUpsideDown);
+  );
+  const strangerThingsService = new StrangerThingsService(
+    strangerThingsRepository,
+    );
+    
+    app.use(cors());
+    const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+    const params = verifyBollean(hereIsTheUpsideDown);
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    params,
   );
 
   res.status(200).json(characters);
