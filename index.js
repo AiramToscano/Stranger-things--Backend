9dfa026557a9742886f417941f93cc9dbb716a58
Airam Toscano <airamlobato@gmail.com>
2022-06-03 18:22:16 -0400

    test

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
-
+//
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 const PORT = process.env.PORT || 3000;
 
